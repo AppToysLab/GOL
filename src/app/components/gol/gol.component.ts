2081ddc2e23d38f61b1733a0b5c52556f4af7f4d
Fox747 <rus747@gmail.com>
2019-08-23 19:50:23 +0300

    SetLifeStatus() Delete neighborsLifes = 0;

--- src/app/components/gol/gol.component.ts
+++ src/app/components/gol/gol.component.ts
@@ -147,7 +147,7 @@ export class GameManager {
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
         let singleCell = this.arrCells[i][j];
-        let neighborsLifes: number = 0;
+        let neighborsLifes: number;// = 0;
         if ((i > 0) && (j > 0)) {
                   neighborsLifes =  this.SummNeighborsLifes(i, j);
                   }
